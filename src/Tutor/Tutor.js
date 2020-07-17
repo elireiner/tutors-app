675cc018bf7f416e26c12628784dc3eaa80bbd93
Eli Reiners <goelyukim@gmail.com>
2020-07-17 14:29:52 -0400

    Remove unnecessary Link

--- src/Tutor/Tutor.js
+++ src/Tutor/Tutor.js
@@ -1,16 +1,15 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 import './Tutor.css'
 
 export default class Tutor extends React.Component {
     render() {
         const { name, price, subject } = this.props
         return (
-            <>
+            <li className="tutor">
                 <p>{name}</p>
                 <p>{price}</p>
                 <p>{subject}</p>
-            </>
+            </li>
         )
     }
 }
