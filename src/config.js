ca6a23439026cd154f1eb6cb695696373a08297c
Eli Reiners <goelyukim@gmail.com>
2020-08-11 15:50:32 -0400

    Add api link

--- src/config.js
+++ src/config.js
@@ -1,5 +1,4 @@
 export default {
-  API_ENDPOINT: 'http://localhost:8000/api/users',
-    //API_ENDPOINT: 'https://gentle-island-35583.herokuapp.com/api/users',
-    API_KEY: process.env.REACT_APP_API_KEY,
+  API_ENDPOINT: 'https://gentle-island-35583.herokuapp.com/api/users',
+  API_KEY: process.env.REACT_APP_API_KEY,
 }
