9cfbfd2a0e541210f25d626b8c9ef867610f21f2
Eli Reiners <goelyukim@gmail.com>
2020-09-14 14:20:52 -0400

    style TutorsPage

--- src/components/TutorPage/TutorPage.js
+++ src/components/TutorPage/TutorPage.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import FormNav from '../FormNav/FormNav'
+import './TutorPage.css'
 
 
 export default class TutorPage extends React.Component {
@@ -16,22 +17,22 @@ export default class TutorPage extends React.Component {
         return (
             <>
                 < FormNav />
-                <h1>Welcome to my page!</h1>
-                <div className="tutor-page">
-                    <p className="tutors-item tutor-page-name">My name is {currentTutor.first_name}</p>
+                <h1 className="tutor-page-header">Welcome to my page!</h1>
+                <div className="tutor-page-info">
+                    <p className="tutors-page-item tutor-page-name">My name is {currentTutor.first_name}.</p>
                     {numberOfSubjects
-                        ? <p className="tutors-item tutor-page-subject">I tutor {currentTutor.subjects}</p>
+                        ? <p className="tutors-page-item tutor-page-subject">I tutor {currentTutor.subjects}</p>
                         :
                         <>
-                            <p className="tutors-item tutor-page-subject">I tutor the following subjects:</p>
+                            <p className="tutors-page-item tutor-page-subject">I tutor the following subjects:</p>
 
                             <ul>
-                                {currentTutor.subjects.map(subject => <li>{subject}</li>)}
+                                {currentTutor.subjects.map(subject => <li className="tutor-page-subject">{subject}</li>)}
                             </ul>
                         </>
                     }
-                    <p className="tutors-item tutor-page-price">My fee is {currentTutor.fee} dollars</p>
-                    <p className="tutors-item tutor-page-email">Please email me <a href={`mailto:${currentTutor.email}`}>here</a></p>
+                    <p className="tutors-page-item tutor-page-price">My fee is {currentTutor.fee} dollars per session.</p>
+                    <p className="tutors-page-item tutor-page-email">Please email me <a href={`mailto:${currentTutor.email}`}>here</a>.</p>
                 </div>
             </>
         )
