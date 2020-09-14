88f37f689a872939d9eddf5e0e246a629af2f27a
Eli Reiners <goelyukim@gmail.com>
2020-09-14 08:40:59 -0400

    Add new api url

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
-  API_ENDPOINT: 'https://gentle-island-35583.herokuapp.com/api/users',
+  API_ENDPOINT: `https://blooming-brushlands-02540.herokuapp.com/api/users`,
   API_KEY: process.env.REACT_APP_API_KEY,
 }
