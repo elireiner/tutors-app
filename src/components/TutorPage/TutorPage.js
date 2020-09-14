81b0fefe3dae24b616351890ac538023ceabb89e
Eli Reiners <goelyukim@gmail.com>
2020-09-14 08:39:02 -0400

    Use props instead of state Add nav

--- src/components/TutorPage/TutorPage.js
+++ src/components/TutorPage/TutorPage.js
@@ -1,55 +1,38 @@
 import React from 'react';
+import FormNav from '../FormNav/FormNav'
 
 
 export default class TutorPage extends React.Component {
-    state = {
-        currentTutor: {
-            tutorId: null,
-            first_name: null,
-            subjects: [],
-            fee: null
-        }
-
-    }
-
-    componentDidMount() {
-        
-        let tutorId = Number(this.props.match.params.tutorId)
-        this.setState({
-            currentTutor: {
-                ...this.state.currentTutor,
-                tutorId
-            }
-
-        })
-    }
-
-    static getDerivedStateFromProps(nextProps, prevState) {
-
-        //Since initially props.tutors isn't filled with tutors, check if props.tutors is already filled
-        if (nextProps.tutors.length > 0) {
-
-            //Get only the tutor that matches the route
-            let currentTutor = nextProps.tutors.find(tutor => tutor.user_id === prevState.currentTutor.tutorId)
-
-            //Check if the state needs to be updated 
-            if (prevState.currentTutor !== nextProps.tutors) {
-                return {
-                    currentTutor
-                }
-            }
-        }
-    }
-
 
     render() {
-        console.log(this.state.currentTutor)
+
+        if (!this.props.tutors) {
+            return null
+        }
+
+        const currentTutor = this.props.tutors.find(tutor => tutor.user_id === Number(this.props.match.params.tutorId))
+        const numberOfSubjects = currentTutor.subjects.length < 2
+
         return (
-            <li className="tutor">
-                <p className="tutors-item tutor-name">{this.state.currentTutor.first_name}</p>
-                <p className="tutors-item tutor-subject">{this.state.currentTutor.subjects}</p>
-                <p className="tutors-item tutor-price">{this.state.currentTutor.fee}</p>
-            </li>
+            <>
+                < FormNav />
+                <h1>Welcome to my page!</h1>
+                <div className="tutor">
+                    <p className="tutors-item tutor-name">My name is {currentTutor.first_name}</p>
+                    {numberOfSubjects
+                        ? <p className="tutors-item tutor-subject">I tutor {currentTutor.subjects}</p>
+                        :
+                        <>
+                            <p className="tutors-item tutor-subject">I tutor the following subjects:</p>
+
+                            <ul>
+                                {currentTutor.subjects.map(subject => <li>{subject}</li>)}
+                            </ul>
+                        </>
+                    }
+                    <p className="tutors-item tutor-price">My fee is {currentTutor.fee} dollars</p>
+                </div>
+            </>
         )
     }
 }
